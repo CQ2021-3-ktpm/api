1b2af2f69e75e480da6bc24cca350f7e524a24ba
hkhangus <huukhangtc@gmail.com>
2024-12-01 23:03:31 +0700

    chore: add /health endppoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,6 +10,11 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get('health')
+  health(): string {
+    return "I'm alive!";
+  }
+
   @Get('hello/:name')
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHelloName(name);
