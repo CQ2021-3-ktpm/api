b2f6cb0bcf9bf215e96eaeed0d2eccea9dcaf5ed
hkhangus <huukhangtc@gmail.com>
2025-01-02 00:23:39 +0700

    chore: add public route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PublicRoute } from './decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @PublicRoute(true)
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('health')
+  @PublicRoute(true)
   health(): string {
     return "I'm alive!";
   }
